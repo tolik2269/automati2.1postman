246881941f603d34fc598c817e3b80e31e481246
Anatoly Balbutsky <bal.anatolii1985@mail.ru>
2023-06-24 17:28:37 +0300

    v6

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -17,7 +17,7 @@ public class PostmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data",equalTo("аапек"))
+                .body("data",equalTo("Hi"))
         ;
     }
 }
